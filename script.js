eeb22c635b5d5b91d6897642546bee94a60089fb
Marius Ramdahl <mariusramdahl@Mariuss-MacBook-Pro.local>
2020-12-15 14:08:37 +0100

    Javascrip ferdig

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 window.addEventListener("load", sidenVises);
 
-function sidenVises(){
-    console.log("sidenVises")
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknapp").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    console.log("toggleMenu");
+
+    document.querySelector("#links1").classList.toggle("hidden");
+    document.querySelector("#links2").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#links1").classList.contains("hidden");
+
+    if (erSkjult == true){
+        document.querySelector("#menuknapp").textContent = "☰"
+    } else {
+        document.querySelector("#menuknapp").textContent = "x"
+    }
 }
